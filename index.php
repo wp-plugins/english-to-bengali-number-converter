59d5293e34b65e9930fab3aa4ef9eb8b0a659ecb
ruhul.me <ruhul.me@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 09:43:16 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/english-to-bengali-number-converter/tags/1.1.1@1062923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,46 @@
+﻿<?php
+/*
+Plugin Name: English To Bengali Number Converter
+Plugin URI:  http://www.timenews24.com/plugin/English-To-Bengli-Number-Convert.zip
+Description: This plugin will help users  convert English to Bengali Date,Time and All Numbers  with pure Php.
+Author: Ruhul Amin
+Author URI: http://ruhul.me
+Version: 1.2.5
+*/
+
+ /* Adding plugin convert section  */
+	add_filter('current_time', 'Bengali_DTN');
+	add_filter('the_date', 'Bengali_DTN');
+	add_filter('the_time', 'Bengali_DTN');
+	add_filter('the_views', 'Bengali_DTN');
+	add_filter('page_number', 'Bengali_DTN');
+	add_filter('comments_number', 'Bengali_DTN');;
+	add_filter('get_comment_date', 'Bengali_DTN');
+	add_filter('get_comment_time', 'Bengali_DTN');
+	add_filter('number_format_i18n', 'Bengali_DTN', 10, 1);
+	add_filter('pre_date_i18n', 'Bengali_DTN', 10, 2); 
+    add_filter( 'get_the_time', 'Bengali_DTN' );
+    add_filter( 'get_the_date', 'Bengali_DTN' );
+    add_filter( 'get_comment_count', 'Bengali_DTN' );
+    add_filter( 'get_archives_link', 'Bengali_DTN' );
+    add_filter( 'wp_list_categories', 'Bengali_DTN' );
+	
+function Bengali_DTN($NRS){
+	$englDTN = array
+			('1','2','3','4','5','6','7','8','9','0',
+			'Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday',
+			'Sat','Sun','Mon','Tue','Wed','Thu','Fri',
+			'am','pm','at','st','nd','rd','th',
+			'January','February','March','April','May','June','July','August','September','October','November','December',
+			'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+			$bangDTN = array
+			('১','২','৩','৪','৫','৬','৭','৮','৯','০',
+			'শনিবার','রবিবার','সোমবার','মঙ্গলবার','বুধবার','বৃহস্পতিবার','শুক্রবার',
+			'শনি','রবি','সোম','মঙ্গল','বুধ','বৃহঃ','শুক্র',
+			'পূর্বাহ্ণ','অপরাহ্ণ','','','','','',
+			'জানুয়ারি','ফেব্রুয়ারি','মার্চ','এপ্রিল','মে','জুন','জুলাই','আগস্ট','সেপ্টেম্বর','অক্টোবর','নভেম্বর','ডিসেম্বর',
+			'জানু','ফেব্রু','মার্চ','এপ্রি','মে','জুন','জুলা','আগ','সেপ্টে','অক্টো','নভে','ডিসে');
+			$converted = str_replace($englDTN, $bangDTN, $NRS);
+			return $converted; 
+			}
+?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== English To Bengali Number Convert ===
+Contributors: Ruhul Amin
+Donate link: 
+Tags: Bengali Date,Bengali time,Bengali all number,
+Requires at least: 3.00
+Tested up to: 3.9.1
+Stable tag: 1.2.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin will help users  convert English to Bengali Date,Time and All Numbers  with pure Php.
+
+== Description ==
+
+This plugin will help users  convert English to Bengali Date,Time and All Numbers  with pure Php.
+
+Live Demo: http://pchelpline24.com/
+
+Plugin Features
+
+* Convert English to Bengali Date.
+* Convert English to Bengali Time.
+* Convert English to Bengali Comments Numbers.
+* Convert English to Bengali All Numbers.
+* Small in Size.
+* Many More..
+
+== Installation ==
+
+= Using The Wordpress Dashboard =
+
+1. First go to the 'Add New' Plugin Dashboard
+2. Select `English-To-Bengali-Number-Convert.zip` file from your computer
+3. Simply Upload
+4. Activate the plugin via the Plugin Section of WordPress Dashbaord
+5. After Activate the plugin Automatically Convert English to Bengali.
+
+= Using FTP =
+
+1. Extract `English-To-Bengali-Number-Convert.zip` to your computer
+2. Upload the `Bengali-Date-Time-Numbers` directory to your `wp-content/plugins` directory
+3. Activate the plugin on the WordPress Plugin Dashbaord
+4. As this plugin supports All theme. 
+
+== Frequently asked questions ==
+
+= Do I need to know any coding to setup and use the plugin? =
+
+No, you do not need to know any kind of coding to setup this plugin. You can easily do that just clicking Automatically Convert English to Bengali.
+
+== Screenshots ==
+1. "Bengali Date and time Numbers" Convert in front end
+2. "Bengali Date and time Numbers" Convert  in Admin Option Panel
+
+
+
+== Changelog ==
+
+= 1.1 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+There is currently no upgrade notice for the users.
